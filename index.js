73b92161be7918e331d65bad74e17ca6c0df0425
Alvito Raidhy <alvito.raidhy21@gmail.com>
2019-06-07 09:00:49 +0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,122 @@
+const assert = require('assert');
+
+const plug_parent = Symbol('plug_parent');
+const plug_name = Symbol('plug_name');
+const required_plugs = Symbol('required_plugs');
+const plugs = Symbol('plugs');
+
+const isString = function(str) {return typeof str === 'string' || str instanceof String;}
+const isPlug = function(instance) {return instance instanceof Plugger}
+
+class Plugger {
+  constructor(name) {
+    if (!isString(name)) {
+      throw new TypeError('Plug name must be string!');
+    }
+
+    this[plug_parent] = null
+    this[plug_name] = name;
+    this[plugs] = {};
+    this[required_plugs] = [];
+  }
+
+  getName() {return this[plug_name];}
+  setName(name) {
+    if (!isString(name)) {
+      throw new TypeError('Plug name must be string!');
+    }
+
+    this[plug_name] = name;
+    return true;
+  }
+
+  getParent() {return this[plug_parent];}
+  setParent(parent) {
+    if (!isPlug(parent)) {
+      throw new TypeError('Parent must be a Plugger instance!')
+    }
+    this[plug_parent] = parent;
+    return true;
+  }
+
+  getPlugs() {return this[plugs];}
+
+  addPlug(plug) {
+    if (isString(plug)) {
+      plug = require(plug);
+      if (!isPlug(plug)) {
+        throw new Error('Module must exports a Plugger instance!');
+      }
+    }
+    else if (!isPlug(plug)) {
+      throw new TypeError('Must be a Plugger instance or a module path!');
+    }
+
+    var name = plug.getName();
+    if (!Object.keys(this[plugs]).includes(name)) {
+      var required = plug.getRequiredPlugs();
+      for (var x in required) {
+        var required_name = required[x].getName();
+        if (!this[plugs][required_name]) {
+          throw new Error('Required Plug not found! -> ' + required_name);    
+        }
+      }
+      this[plugs][name] = plug;
+      plug.setParent(this);
+      return true;
+    }
+    else {
+      throw new Error('A Plug with the same name already exists!');
+    }
+  }
+  removePlug(plug) {
+    if (isString(plug)) {
+      plug = require(plug);
+      if (!isPlug(plug)) {
+        throw new Error('Module must exports a Plugger instance!');
+      }
+    }
+    else if (!isPlug(plug)) {
+      throw new TypeError('Must be a Plugger instance or a module path!');
+    }
+
+    var name = plug.getName();
+    if (Object.keys(this[plugs]).includes(name)) {
+      delete this[plugs][name];
+      return true;
+    }
+    else {
+      throw new Error('Plug instance not found!');
+    }
+  }
+  getPlug(name) {
+    if (!isString(name)) {
+      throw new TypeError('Plug name must be string!');
+    }
+
+    if (Object.keys(this[plugs]).includes(name)) {
+      return this[plugs][name];
+    }
+    else {
+      return null;
+    }
+  }
+
+  getRequiredPlugs() {
+    return this[required_plugs];
+  }
+  removeRequiredPlug(name) {
+    if (this[required_plugs].indexOf(name) > -1) {
+      this[required_plugs].splice(name, 1);
+    }
+    return true;
+  }
+  requirePlug(name) {
+    if (!this[required_plugs].indexOf(name) > -1) {
+      this[required_plugs].push(name);
+    }
+    return true;
+  }
+}
+
+module.exports = Plugger;
---
+++ test/plugger.js
@@ -0,0 +1,86 @@
+const assert = require('assert');
+const Plugger = require('../index');
+
+describe('The Plugger class', function() {
+  it('should be a class (Object)', function() {
+    assert.strictEqual(Plugger instanceof Object, true);
+  });
+  it('should have \'getName\' method', function() {
+    assert.strictEqual(Plugger.prototype.getName instanceof Function, true);
+  });
+  it('should have \'setName\' method', function() {
+    assert.strictEqual(Plugger.prototype.setName instanceof Function, true);
+  });
+  it('should have \'getParent\' method', function() {
+    assert.strictEqual(Plugger.prototype.getParent instanceof Function, true);
+  });
+  it('should have \'setParent\' method', function() {
+    assert.strictEqual(Plugger.prototype.setParent instanceof Function, true);
+  });
+  it('should have \'getPlugs\' method', function() {
+    assert.strictEqual(Plugger.prototype.getPlugs instanceof Function, true);
+  });
+  it('should have \'addPlug\' method', function() {
+    assert.strictEqual(Plugger.prototype.addPlug instanceof Function, true);
+  });
+  it('should have \'removePlug\' method', function() {
+    assert.strictEqual(Plugger.prototype.removePlug instanceof Function, true);
+  });
+  it('should have \'getPlug\' method', function() {
+    assert.strictEqual(Plugger.prototype.getPlug instanceof Function, true);
+  });
+  it('should have \'getRequiredPlugs\' method', function() {
+    assert.strictEqual(Plugger.prototype.getRequiredPlugs instanceof Function, true);
+  });
+  it('should have \'removeRequiredPlug\' method', function() {
+    assert.strictEqual(Plugger.prototype.removeRequiredPlug instanceof Function, true);
+  });
+  it('should have \'requirePlug\' method', function() {
+    assert.strictEqual(Plugger.prototype.requirePlug instanceof Function, true);
+  });
+});
+
+var parent = new Plugger('parent');
+var children = new Plugger('children');
+var children_same_name = new Plugger('children');
+var children2 = new Plugger('children2');
+var required_plug = new Plugger('required');
+
+describe('The Plugger instance', function() {
+  it('should be able to return a correct name (getName)', function() {
+    assert.strictEqual(parent.getName(), 'parent');
+  });
+  it('should be able to change name (setName)', function() {
+    parent.setName('new_name');
+    assert.strictEqual(parent.getName(), 'new_name');
+  });
+  it('should be able to add other Plug (addPlug)', function() {
+    assert.strictEqual(parent.addPlug(children), true);
+    assert.strictEqual(parent.addPlug(children2), true);
+    assert.strictEqual(children.getParent(), parent);
+  });
+  it('shouldn\'t be able to add other Plug with the same name as another inserted Plug (addPlug)', function() {
+    var result;
+    try {
+      parent.addPlug(children_same_name);
+      result = 'Plug inserted'
+    }
+    catch(err) {
+      result = 'Error thrown'
+    }
+    assert.strictEqual(result, 'Error thrown');
+  });
+
+  it('should be able to get inserted Plug (getPlug)', function() {
+    assert.strictEqual(parent.getPlug('children'), children);
+  });
+
+  it('should be able require a Plug (requirePlug)', function() {
+    assert.strictEqual(parent.requirePlug('required'), true);
+  });
+
+  it('should be able remove a required Plug (removeRequiredPlug)', function() {
+    parent.removeRequiredPlug('required');
+    assert.deepStrictEqual(parent.getRequiredPlugs(), []);
+  });
+});
