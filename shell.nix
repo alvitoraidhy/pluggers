b7d249bd133d9b2cdf4d1c67e9732eac0dc2d4d1
Alvito Raidhy <alvito.raidhy@gmail.com>
2022-03-26 21:10:48 +0700

    feat: update dependencies

--- shell.nix
+++ shell.nix
@@ -4,11 +4,11 @@
 pkgs.mkShell {               # mkShell is a helper function
   name="dev-environment";    # that requires a name
   buildInputs = [            # and a list of packages
-    pkgs.nodejs-10_x         # minimum supported NodeJS
+    pkgs.nodejs-12_x         # minimum supported NodeJS
   ];
   shellHook =                # bash to run when you enter the shell 
   ''
-    echo "Node.js v10";             
+    echo "Node.js v12";             
     echo "Start developing...";
   '';
 }
